9f66925178a74e241158e1369a7e68cb60171703
EvgenSk <evgensk@gmail.com>
2019-06-17 23:02:39 +0700

    initial skeleton commit

---
+++ OrleansCompoundGrainStorage/Storage/CompoundGrainStorage.cs
@@ -0,0 +1,33 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Storage
+{
+    public class CompoundGrainStorage : IGrainStorage, ILifecycleParticipant<ISiloLifecycle>
+    {
+        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Participate(ISiloLifecycle lifecycle)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
