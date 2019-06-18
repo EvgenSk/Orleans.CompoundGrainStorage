7f4b684294e08e6a8daca02ea8746f9a03dd6434
EvgenSk <evgensk@gmail.com>
2019-06-18 23:04:56 +0700

    basic CompoundGrainStorage implementation

--- OrleansCompoundGrainStorage/Storage/CompoundGrainStorage.cs
+++ OrleansCompoundGrainStorage/Storage/CompoundGrainStorage.cs
@@ -1,33 +1,83 @@
 ﻿using Orleans;
+using Orleans.Configuration;
 using Orleans.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Orleans.Storage
 {
     public class CompoundGrainStorage : IGrainStorage, ILifecycleParticipant<ISiloLifecycle>
     {
-        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
-        {
-            throw new NotImplementedException();
-        }
+        string Name { get; }
+        CompoundGrainStorageOptions Options { get; }
+        IServiceProvider ServiceProvider { get; }
 
-        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        IGrainStorage Cache { get; set; }
+        IGrainStorage Storage { get; set; }
+
+        bool IsReadOnly { get; set; }
+        public bool UpdateCache { get; }
+
+        public CompoundGrainStorage(string name, CompoundGrainStorageOptions options, IServiceProvider serviceProvider)
         {
-            throw new NotImplementedException();
+            Name = name;
+            Options = options;
+
+            IsReadOnly = Options.ReadOnly;
+            UpdateCache = Options.UpdateCache;
+
+            ServiceProvider = serviceProvider;
+        }
+        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState) =>
+            Task.WhenAll(
+                Cache.ClearStateAsync(grainType, grainReference, grainState), 
+                Storage.ClearStateAsync(grainType, grainReference, grainState)
+                );
+
+        public async Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            try
+            {
+                await Cache.ReadStateAsync(grainType, grainReference, grainState);
+            }
+            catch
+            {
+                await Storage.ReadStateAsync(grainType, grainReference, grainState);
+
+                if (!UpdateCache)
+                    return;
+
+                var isReadOnly = IsReadOnly;
+                IsReadOnly = false;
+                await WriteStateAsync(grainType, grainReference, grainState);
+                IsReadOnly = isReadOnly;
+            }
         }
 
         public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+                return Task.CompletedTask;
+
+            return Cache.WriteStateAsync(grainType, grainReference, grainState);
         }
 
         public void Participate(ISiloLifecycle lifecycle)
         {
-            throw new NotImplementedException();
+            lifecycle.Subscribe(OptionFormattingUtilities.Name<CompoundGrainStorage>(Name), ServiceLifecycleStage.ApplicationServices, Init, Close);
         }
 
+        Task Init(CancellationToken ct)
+        {
+            Cache = ServiceProvider.GetRequiredServiceByName<IGrainStorage>(Options.CacheName);
+            Storage = ServiceProvider.GetRequiredServiceByName<IGrainStorage>(Options.StorageName);
+
+            return Task.CompletedTask;
+        }
+
+        Task Close(CancellationToken ct) => Task.CompletedTask;
     }
 }
--- OrleansCompoundGrainStorage/Storage/CompoundGrainStorageOptions.cs
+++ OrleansCompoundGrainStorage/Storage/CompoundGrainStorageOptions.cs
@@ -6,5 +6,10 @@ namespace Orleans.Configuration
 {
     public class CompoundGrainStorageOptions
     {
+        public string CacheName { get; set; }
+        public string StorageName { get; set; }
+
+        public bool ReadOnly { get; set; } = false;
+        public bool UpdateCache { get; set; } = true;
     }
 }
