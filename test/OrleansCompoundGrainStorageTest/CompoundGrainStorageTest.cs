248f186bc3c27a2cbe343aa64a62f73cd6bcdf41
EvgenSk <evgensk@gmail.com>
2019-06-23 22:59:42 +0700

    separate code and tests

---
+++ test/OrleansCompoundGrainStorageTest/CompoundGrainStorageTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+using Orleans;
+using Orleans.TestingHost;
+
+using OrleansCompoundGrainStorage;
+
+namespace OrleansCompoundGrainStorage.Test
+{
+    public class CompoundGrainStorageTest
+    {
+        [Fact]
+        public void ReadStateAsync_AddToStorageReadFromCache_ReturnsCorrectValue()
+        {
+
+        }
+    }
+}
